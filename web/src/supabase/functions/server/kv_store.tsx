753e9f4a7f01487e6114fe0c6dead6220f2d5383
sergiogd8 <133170376+sergiogd8@users.noreply.github.com>
2025-12-17 23:50:42 +0100

    Delete web directory

--- web/src/supabase/functions/server/kv_store.tsx
+++
@@ -1,8 +0,0 @@
-// Este archivo ya no se usa - El almacenamiento ahora es directo con Supabase PostgreSQL
-
-export default function() {
-  return new Response(
-    JSON.stringify({ message: 'Este KV store ya no se usa.' }),
-    { headers: { 'Content-Type': 'application/json' } }
-  );
-}
